72ebfa55246e07f399084484ac504666f673f4d9
Kevin Cui <krazycavin@gmail.com>
2019-01-22 18:41:51 +0800

    Add main script

---
+++ xe-scraper.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+    const chrome = '/usr/bin/chromium'
+    const isheadless = true
+    const FROM = process.argv[2]
+    const TO = process.argv[3]
+    const AMOUNT = process.argv[4]
+    const URL= 'https://www.xe.com/currencyconverter/convert/?Amount=' + AMOUNT + '&From=' + FROM + '&To=' + TO
+    const SELECTOR = '.converterresult-conversionWrap';
+
+    const browser = await puppeteer.launch({executablePath: chrome, headless: isheadless});
+    const page = await browser.newPage();
+
+    await page.goto(URL, {timeout: 60000, waitUntil: 'domcontentloaded'});
+    await page.waitForSelector(SELECTOR)
+
+    const data = await page.evaluate(() => {
+        return {
+            fromAmount: document.querySelector('.converterresult-fromAmount').textContent,
+            fromCurrency: document.querySelector('.converterresult-fromCurrency').textContent,
+            toAmount: document.querySelector('.converterresult-toAmount').textContent,
+            toCurrency: document.querySelector('.converterresult-toCurrency').textContent
+        };
+    });
+    console.log(data.fromAmount + data.fromCurrency + ' = ' + data.toAmount + data.toCurrency);
+
+    await browser.close();
+})();
